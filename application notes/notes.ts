51a8ba38721e897104c36200685e543fd47cf768
Hariom <harim987316@gmail.com>
2021-12-16 13:28:01 +0530

    code check-in

---
+++ application notes/notes.ts
@@ -0,0 +1,92 @@
+/**
+ # React Components
+  - React is JavaScript library to building user interfaces
+  - react and react-dom both library combine to make a working react
+  - All userinterfaces in react are made up of component.
+
+ # Component
+  - Reusability(Don't repeat yourself)  and  separation of concerns(helps in small and managable code)
+  - Split big chrunks of code into multiple smaller functions.
+
+ # Component Build
+  - HTML    CSS     JavaScript
+  - React allows you to create re-usable and reactive components consisting of HTML and JavaScript (and css)
+  - React uses a declarative approach.
+  - Define the desired target state and let react figure out the actual javascript DOM instructions.
+----------------------------------------------------------------------------------------------------------------
+ # Creating a react project
+  - npx create-react-app 'project_name'
+  - cd 'project_name'
+  - npm start
+
+ # index.html (public)
+   - This is the only file which is rendered in the browser
+   - On this single file we import the finished react application in this file.
+
+ # JSX (JavaScript XML)
+   - JavaScript code in XML(HTML)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
